6efbe8b440df78cdbf5367bda32d66c0f1be718b
FrancoisDoue <doue.francois@outlook.com>
2023-11-22 08:45:00 +0100

    exo -suite

---
+++ 07_Javascript/exo_js_suite_nbr_cons/main.js
@@ -0,0 +1,46 @@
+const USER_NBR = Number(prompt("Saisir nombre :"));
+let msg = "", endLog = "", step = 1, loopNbr = USER_NBR;
+
+for (let cnt = 0; cnt < USER_NBR / 2 + 1; loopNbr -= ++cnt) {
+    if (loopNbr > 0) {
+        msg += (cnt !== 0) ? `${cnt} + ` : "";
+    } else {
+        if (loopNbr === 0) endLog += `${USER_NBR} = ${msg + cnt}\n`;
+        cnt = ++step;
+        msg = "";
+        loopNbr = USER_NBR;
+    }
+}
+console.log(endLog);
+
+// alert(log);
+
+// fait dans le train pour soigner mon ego
+// let msg = "", log = "";
+// // let cpt = 0;
+// let step = 1;
+
+// let nbrUser = Number(prompt("Saisir nombre :"));
+// let testedNbr = nbrUser;
+/**
+ * A chaque tour, on soustrait cpt à testedNbr
+ * S'il tombe pile à 0, on peut ajouter la chaine de charactère au log final
+ * ou tout réinitialiser avec un cpt commençant à 2 à la boucle suivante
+ */
+// while(cpt <= nbrUser/2){ // totalement possible avec une boucle for
+//     msg += ++cpt; // incrémentation avant affectation
+//     testedNbr -= cpt;
+
+//     if(testedNbr === 0){
+//         log += `${nbrUser} = ${msg}\n`;
+//         testedNbr = nbrUser;
+//         cpt = ++step;
+//         msg = "";
+//     }else msg += " + ";
+
+//     if(testedNbr < 0){
+//         cpt = step++;
+//         msg = "";
+//         testedNbr = nbrUser;
+//     }
+// }
