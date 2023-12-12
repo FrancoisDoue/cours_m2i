f23f263c036a3e92e95dcdfab2bc11c5ac8e9af1
FrancoisDoue <doue.francois@outlook.com>
2023-12-12 11:43:02 +0100

    exo userlist

--- 09_vue_js/demo_vue_part_2/exo-vue-components/src/App.vue
+++ 09_vue_js/demo_vue_part_2/exo-vue-components/src/App.vue
@@ -2,16 +2,12 @@
 import UserList from './components/UserList.vue';
 
 
-// import TodoList from './components/TodoList.vue';
-
-
 </script>
 
 <template>
   <div class="d-flex flex-column align-items-center">
     <UserList />
   </div>
-  <!-- <TodoList> -->
 </template>
 
 <style scoped>
