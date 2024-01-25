0620e51291310ec8c91c35db4a15a983d7bf0aa2
FrancoisDoue <doue.francois@outlook.com>
2024-01-25 08:56:14 +0100

    test mongoose - 25/01 am

---
+++ 14_express_js/test_express_mongodb/app.js
@@ -0,0 +1,45 @@
+import 'dotenv/config'
+import express from 'express';
+import router from './src/routes/index.js';
+import mongoose, { Schema } from 'mongoose';
+
+
+
+const mongo = async() => await mongoose.connect(process.env.MONGO_URI)
+mongo().catch(err => console.log(err))
+
+const PORT = process.env.PORT
+const app = express()
+// mongoose
+//     .connect(process.env.MONGO_URI)
+//     .then(result => app.listen(5000, () => `MongoDB ready`))
+//     .catch(err => console.log(err))
+
+app.use(express.json())
+app.use('/', router)
+
+app.listen(PORT, () => console.log(`SERVER IS LISTENING ON PORT ${PORT}`))
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+
+// console.log(newProduct)
+
+// productSchema.methods.getName = function getName(){
+//     console.log(this.name)
+//     return this.name
+// }
+
+const newProduct = new Product({name: "Telephone", price: 200})
+
+// findSimilarTypes(cb) {
+//     return mongoose.model('Animal').find({ type: this.type }, cb);
+//   }
+// }
+// });
+// newProduct.getName()
