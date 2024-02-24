899678254662dbc4f2dc2d8f3a95e962af415684
FrancoisDoue <doue.francois@outlook.com>
2024-02-24 19:48:59 +0100

    style and ui improvements. Refactoring add/delete cart functions

--- 16_react_js/exercices/10_ecommerce/src/components/Header.jsx
+++ 16_react_js/exercices/10_ecommerce/src/components/Header.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProductContext from '../context/ProductContext';
 
 const Header = ({cartBtnAction}) => {
+    const {cart} = useContext(ProductContext)
     return (
         <header className='bg-dark text-light p-1 px-4 d-flex justify-content-between'>
             <h1>E-C<span><i className="bi bi-globe2"></i></span>MMERCE</h1>
             <div className='align-self-center mx-4'>
                 <button 
-                    className='btn btn-primary'
+                    className='btn btn-primary position-relative'
                     onClick={cartBtnAction}
                 >
-                    <span><i className="bi bi-cart"></i></span> Panier
+                    
+                    <span className=''>
+                        <i className="bi bi-cart"></i>
+                        Panier
+                    </span>
+                    {!!cart.length &&
+                        <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill text-bg-warning'>
+                            {cart.reduce((total, e) => total + e.qte, 0)}
+                        </span>
+                    }
                 </button>
             </div>
         </header>
