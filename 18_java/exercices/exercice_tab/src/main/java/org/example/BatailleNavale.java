dbceede0e00c943af17b29481a5057ba21157f02
FrancoisDoue <doue.francois@outlook.com>
2024-04-08 00:34:07 +0200

    java - améliorations bataille navale

--- 18_java/exercices/exercice_tab/src/main/java/org/example/BatailleNavale.java
+++ 18_java/exercices/exercice_tab/src/main/java/org/example/BatailleNavale.java
@@ -1,34 +1,35 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BatailleNavale {
     public static void Main (){
 
-        String[] abscysse = {"a","b","c","d","e","f","g","h","i","j"};
+        String[] vPositions = {"a","b","c","d","e","f","g","h","i","j"};
         int[] ships = {5, 4, 3, 3, 2};
         int[][][] ocean = new int[2][10][10];
         int totalPoints = 17;
         int points = 0, enemyPoints = 0;
         // ships placement
-        for (int[][] seas : ocean){
+        for (int[][] sea : ocean){
             for (int ship : ships){
                 boolean isHorizontal = Math.random() > .5 ;
-                int randomLine = (int)(Math.random()*(seas.length - ship));
-                int randomCol = (int)(Math.random()*(seas.length - ship));
+                int randomLine = (int)(Math.random()*(sea.length - ship));
+                int randomCol = (int)(Math.random()*(sea.length - ship));
                 // SCAN POSITIONS
-                for (int i = 0; i < seas.length; i++) {
-                    int box = (isHorizontal) ? seas[randomLine][i] : seas[i][randomCol];
+                for (int i = 0; i < sea.length; i++) {
+                    int box = (isHorizontal) ? sea[randomLine][i] : sea[i][randomCol];
                     if (box == 1 ){
                         if(isHorizontal) {
                             randomLine += ship;
-                            if(randomLine > seas.length -1) {
-                                randomLine = (int)(Math.random()*(seas.length - ship));
+                            if(randomLine > sea.length -1) {
+                                randomLine = (int)(Math.random()*(sea.length - ship));
                             }
                         } else {
                             randomCol += ship;
-                            if(randomCol > seas.length -1) {
-                                randomCol = (int)(Math.random()*(seas.length - ship));
+                            if(randomCol > sea.length -1) {
+                                randomCol = (int)(Math.random()*(sea.length - ship));
                             }
                         }
                         i--;
@@ -37,9 +38,9 @@ public class BatailleNavale {
                 // END SCAN POSITIONS
                 for (int i = 0; i < ship; i++) {
                     if (isHorizontal) {
-                        seas[randomLine][randomCol+i] = 1;
+                        sea[randomLine][randomCol+i] = 1;
                     } else {
-                        seas[randomLine+i][randomCol] = 1;
+                        sea[randomLine+i][randomCol] = 1;
                     }
                 }
             }
@@ -59,7 +60,7 @@ public class BatailleNavale {
                     boxColors[1] = " \u001B[32m■ \u001B[0m";
                 }
                 for(int i = seas.length-1; i >= 0; i--){
-                    StringBuilder displayLine = new StringBuilder(abscysse[i]+" ");
+                    StringBuilder displayLine = new StringBuilder(vPositions[i]+" ");
                     for (int box : seas[i]) {
                         displayLine.append(boxColors[box]);
                     }
@@ -69,43 +70,52 @@ public class BatailleNavale {
             }
             // END DISPLAY
             // USER ACTION
-            System.out.println("Sélectionnez coordonnées [a - j]");
+            System.out.println("Sélectionnez coordonnées [ex: a5]");
             userInput = input.nextLine();
-            for (x = 0; x < abscysse.length; x++) {
-                if(userInput.equals(abscysse[x])) break;
+            System.out.println("Salve tirée vers " + userInput);
+            char[] charArray = userInput.toCharArray();
+            for (x = 0; x < vPositions.length; x++) {
+                if(vPositions[x].charAt(0) == charArray[0]) break;
             }
-            System.out.println("Sélectionnez coordonnées [0 - 9]");
-            y = Math.abs(input.nextInt());
-            x = (x < abscysse.length) ? x : abscysse.length - 1;
-            y = (y < abscysse.length) ? y : abscysse.length - 1;
-            System.out.println(abscysse[x]+"-"+y);
+            y = charArray[1] - '0'; // char in int cast
+            x = (x < vPositions.length) ? x : vPositions.length - 1;
+            y = (y < vPositions.length) ? y : vPositions.length - 1;
             if( ocean[0][x][y] == 1 ){
-                System.out.println("Touché!");
+                System.out.println("\u001B[32mTouché!\u001B[0m");
                 ocean[0][x][y] = 3;
                 points ++;
             } else {
-                System.out.println("Plouf!");
-                ocean[0][x][y] = 2;
+                if (ocean[0][x][y] == 3) {
+                    System.out.println("Inutile de s'acharner!");
+                }else {
+                    System.out.println("Raté!");
+                    ocean[0][x][y] = 2;
+                }
             }
             // END USER ACTION
             // ENEMY ACTION
-            x = (int)(Math.random()*(abscysse.length -1));
-            y = (int)(Math.random()*(abscysse.length -1));
-            System.out.println("x = " + x + ", y = " + y);
+            x = (int)(Math.random()*(vPositions.length -1));
+            y = (int)(Math.random()*(vPositions.length -1));
+            System.out.println("L'adversaire tire en direction de "+vPositions[x]+y);
             if( ocean[1][x][y] == 1 ){
-                System.out.println("Vous êtes touché!");
+                System.out.println("\u001B[31mVous êtes touché!\u001B[0m");
                 ocean[1][x][y] = 3;
                 enemyPoints ++;
             } else {
-                System.out.println("L'ennemi a raté!");
-                ocean[1][x][y] = 2;
+                if (ocean[1][x][y] == 3) {
+                    System.out.println("L'adversaire de s'acharne...");
+                }else {
+                    System.out.println("L'adversaire a raté!");
+                    ocean[1][x][y] = 2;
+                }
             }
             // END ENEMY ACTION
-        } while (points < totalPoints || enemyPoints < totalPoints);
+        } while (points < totalPoints && enemyPoints < totalPoints);
+
         if (points > enemyPoints){
-            System.out.println("Vous avez gagné");
+            System.out.println("\nVous avez gagné");
         } else {
-            System.out.println("Vous avez perdu");
+            System.out.println("\nVous avez perdu");
         }
     }
 }
