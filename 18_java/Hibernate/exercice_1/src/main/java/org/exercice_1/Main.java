3fe686987a3eb89a559ac97545d317168a29d019
FrancoisDoue <doue.francois@outlook.com>
2024-05-28 11:35:31 +0200

    JAVA Hibernate - exercice 1

--- 18_java/Hibernate/exercice_1/src/main/java/org/exercice_1/Main.java
+++ 18_java/Hibernate/exercice_1/src/main/java/org/exercice_1/Main.java
@@ -13,19 +13,18 @@ public class Main {
         SessionFactory factory = HibernateUtil.getFactory();
         Session session = factory.openSession();
 
-        session.getTransaction().begin();
         Product product1 = new Product("marque1", "model1", LocalDate.of(2024,5,25), 10., 5);
         Product product2 = new Product("marque2", "model2", LocalDate.of(2024,5,26), 10., 2);
         Product product3 = new Product("marque3", null, LocalDate.of(2024,5,26), 10., 7);
         Product product4 = new Product("marque4", null, LocalDate.of(2024,5,28), 10., 9);
         Product product5 = new Product("marque5", null, LocalDate.of(2024,5,28), 10., 5);
 
+        session.getTransaction().begin();
         session.save(product1);
         session.save(product2);
         session.save(product3);
         session.save(product4);
         session.save(product5);
-
         session.getTransaction().commit();
 
         Product findedProduct = session.get(Product.class, 2);
