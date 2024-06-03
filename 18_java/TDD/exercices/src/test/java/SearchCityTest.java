e7b4d9156e50dab6bf16c8812d44593e6292b99a
FrancoisDoue <doue.francois@outlook.com>
2024-06-03 18:39:42 +0200

    TDD ex 4.1-1

--- 18_java/TDD/exercices/src/test/java/SearchCityTest.java
+++ 18_java/TDD/exercices/src/test/java/SearchCityTest.java
@@ -1,2 +1,15 @@
+import jdk.jshell.spi.ExecutionControl;
+import org.exercices.exercice_4.NotFoundException;
+import org.exercices.exercice_4.SearchCity;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SearchCityTest {
+
+    private SearchCity searchCity;
+
+    @Test public void shouldThrowNotFoundException_WhenLessOfTwoChar() throws ExecutionControl.NotImplementedException {
+        searchCity = new SearchCity();
+        Assert.assertThrows(NotFoundException.class, () -> searchCity.search("T"));
+    }
 }
