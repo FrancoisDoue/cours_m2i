91de993dafc7987f03ae5d6cce426447b1935914
FrancoisDoue <doue.francois@outlook.com>
2024-07-08 17:30:51 +0200

    java rest save 0807

--- 19_spring/demo/demo_rest/src/main/java/com/example/demo_rest/controller/BaseController.java
+++ 19_spring/demo/demo_rest/src/main/java/com/example/demo_rest/controller/BaseController.java
@@ -1,2 +1,10 @@
-package com.example.demo_rest.controller;public class BaseController {
+package com.example.demo_rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BaseController {
+
 }
