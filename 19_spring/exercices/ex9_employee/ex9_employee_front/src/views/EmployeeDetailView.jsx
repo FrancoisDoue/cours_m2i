78f90446b8cd4bb6b250a40adf260e81616c35fd
FrancoisDoue <doue.francois@outlook.com>
2024-07-10 19:17:45 +0200

    ex9 react employee display

--- 19_spring/exercices/ex9_employee/ex9_employee_front/src/views/EmployeeDetailView.jsx
+++ 19_spring/exercices/ex9_employee/ex9_employee_front/src/views/EmployeeDetailView.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link, useParams } from 'react-router-dom'
 
 const EmployeeDetailView = () => {
+  const {id} = useParams()
+  const {employeeList} = useSelector(state => state.employee)
+  const [employee, setEmployee] = useState(employeeList.find(e => e.id == id))
 
-    const {id} = useParams()
+  useEffect(() => {
+    console.log(employee)
+    setEmployee(employeeList.find(e => e.id == id))
+  }, [employeeList])
+
   return (
-    <div>EmployeeDetailView {id}</div>
+    <div className='row'>
+      <div className="col-lg-6 p-2-md">
+
+        <div className='card shadow-sm'>
+          {employee && <>
+            <div className='card-header'>
+              <h4 className='card-title text-center'> {employee.firstname + " " + employee.lastname} </h4>
+            </div>
+            <ul className='list-group list-group-flush'>
+
+              <li className="list-group-item d-flex justify-content-between bg-light">
+                <span className='w-50 fw-light'>Poste occupé:</span>
+                <span className='w-50 fw-semibold'>{employee.occupation}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                <span className='w-50 fw-light'>ID employé:</span>
+                <span className='w-50'>{employee.identificationNumber}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                <span className='w-50 fw-light'>Téléphone:</span>
+                <span className='w-50'>{employee.phone}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                <span className='w-50 fw-light'>Email:</span>
+                <span className='w-50'>{employee.email}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                <span className='w-50 fw-light'>Adresse postale:</span>
+                <span className='w-50'>{employee.address}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                <span className='w-50 fw-light'>Date de naissance:</span>
+                <span className='w-50'>{employee.birthDate}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                <span className='w-50 fw-light'>Salaire mensuel : </span>
+                <span className='w-50'>{employee.salary} €</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between bg-light">
+                <span className='w-50 fw-light'>Prise de poste le </span>
+                <span className='w-50 fw-semibold'>{employee.contractStart}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between bg-light">
+                <span className='w-50 fw-light'>Fin de contrat le </span>
+                <span className='w-50 fw-semibold'>{employee.contractEnd}</span>
+              </li>
+              {employee?.observation && <li className="list-group-item d-flex flex-column">
+                <span className='fw-light'>Observations notables : </span>
+                <span className='text-wrap'>{employee.observation}</span>
+              </li>
+              }
+              {employee.admin && 
+                <li className="list-group-item bg-warning-subtle text-center fw-semibold">
+                  Cet employé dispose des droits Administrateur
+                </li>
+              }
+            </ul>
+            <div className='card-footer d-flex justify-content-between'>
+              <Link className='link' to={"/"}>Editer les informations</Link>
+            </div>
+            </>}
+        </div>
+
+      </div>
+      <div className="col-lg-6 p-2-md">
+          <div className='card'>
+            {employee.absences ? <div className='text-header'>
+
+            </div> :
+            <div>
+              <h4>Aucune absences notifiées</h4>
+            </div>
+            }
+
+          </div>
+      </div>
+    </div>
   )
 }
 
--- 19_spring/exercices/ex9_employee/ex9_employee_front/src/views/EmployeeListView.jsx
+++ 19_spring/exercices/ex9_employee/ex9_employee_front/src/views/EmployeeListView.jsx
@@ -24,8 +24,12 @@ const EmployeeListView = () => {
                         <p className='fs-5'>{e?.occupation}</p>
                     </div>
                     <div className='col-3 d-flex justify-content-end'>
-                        <a onClick={() => navigate(`/employees/${e.id}`)} className='btn btn-outline-primary my-auto mx-2'>Détails</a>
-                        <a onClick={() => dispatch(deleteEmployee(e.id))} className='btn btn-outline-danger my-auto mx-2'>Plan social</a>
+                        <a onClick={() => navigate(`/employees/${e.id}`)} 
+                            className='btn btn-outline-primary my-auto mx-2'
+                            >Détails</a>
+                        <a onClick={() => dispatch(deleteEmployee(e.id))} 
+                            className='btn btn-outline-danger my-auto mx-2'
+                            >Plan social</a>
                     </div>
                 </div>
             </div>
