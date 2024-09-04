7a64e1745ebf9b53735b0eff20aac3e7c9e5fd28
FrancoisDoue <doue.francois@outlook.com>
2024-09-04 07:46:34 +0200

    review & borrow

--- 22_quarkus/exercices/tp1/_database/script.sql
+++ 22_quarkus/exercices/tp1/_database/script.sql
@@ -6,10 +6,10 @@ CREATE DATABASE IF NOT EXISTS `ms_library_user` ;
 
 USE `ms_library_author`;
 CREATE TABLE IF NOT EXISTS `author` (
-      id BIGINT PRIMARY KEY AUTO_INCREMENT,
-      firstname VARCHAR(255) DEFAULT NULL,
-      lastname VARCHAR(255) DEFAULT NULL,
-      birthDate DATE DEFAULT NULL
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    firstname   VARCHAR(255) DEFAULT NULL,
+    lastname    VARCHAR(255) DEFAULT NULL,
+    birthDate   DATE DEFAULT NULL
 );
 
 INSERT INTO author (firstname, lastname, birthDate)
@@ -18,11 +18,11 @@ VALUES ('Victor', 'Hugo', '1802-02-26'),
 
 USE `ms_library_book`;
 CREATE TABLE IF NOT EXISTS `book` (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255),
-    author_id BIGINT,
-    quantity INT,
-    isbn VARCHAR(255)
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title       VARCHAR(255),
+    author_id   BIGINT,
+    quantity    INT,
+    isbn        VARCHAR(255)
 );
 
 INSERT INTO book (title, quantity, isbn, author_id)
@@ -33,13 +33,43 @@ VALUES ('Les misérables', 2, 'guylmyif', 1),
 USE `ms_library_user`;
 
 CREATE TABLE IF NOT EXISTS `account` (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255),
+    username    VARCHAR(255) UNIQUE NOT NULL,
+    password    VARCHAR(255)
 );
 
 INSERT INTO account (name, username, password)
 VALUES ('Georges Abitbol', 'gabitbol', '123456'),
        ('Bernard', 'nanard', '123456'),
        ('Jean-eude', 'eeeuh', '123456');
+
+USE `ms_library_borrow`;
+
+CREATE TABLE IF NOT EXISTS `borrow` (
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    borrow_date DATE DEFAULT NULL,
+    return_date DATE DEFAULT NULL,
+    book_id     BIGINT DEFAULT NULL,
+    user_id     BIGINT DEFAULT NULL
+);
+
+INSERT INTO borrow (user_id, book_id, borrow_date, return_date)
+VALUES (1,1, '2024-08-15', NULL),
+       (3,3, '2024-08-01', '2024-08-08');
+
+USE `ms_library_review`;
+
+CREATE TABLE IF NOT EXISTS `review` (
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id     BIGINT DEFAULT NULL,
+    book_id     BIGINT DEFAULT NULL,
+    notation    INT DEFAULT 0,
+    content     VARCHAR(255) DEFAULT NULL
+);
+
+INSERT INTO review (user_id, book_id, notation, content)
+VALUES (1, 1, 7, 'Plutôt pas mal'),
+       (1, 2, 5, 'Le héros meurt à la fin'),
+       (3, 3, 10, 'trop bieng');
+
