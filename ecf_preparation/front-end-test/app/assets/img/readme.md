1f38734af4da6eb2fd843a43ce715d2eaf4a02e4
FrancoisDoue <doue.francois@outlook.com>
2024-09-09 07:11:32 +0200

    delete efc_preparation

--- ecf_preparation/front-end-test/app/assets/img/readme.md
+++
@@ -1,1 +0,0 @@
-here's public images
--- ecf_preparation/front-end-test/postcss.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  plugins: {
-    // 'postcss-nested': {},
-    'postcss-import': {},
-    'tailwind/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
