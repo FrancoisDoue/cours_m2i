737dee1a02cb7e277d41ddfa7adf12f7939d0a89
FrancoisDoue <doue.francois@outlook.com>
2024-09-17 17:31:37 +0200

    quarkus kafka exo start

---
+++ 22_quarkus/demo_kafka/micros/account-service/src/main/java/com/example/accountservice/resource/AccountResource.java
@@ -0,0 +1,35 @@
+package com.example.accountservice.resource;
+
+import com.example.accountservice.entity.Account;
+import com.example.accountservice.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountResource {
+
+    private final AccountService accountService;
+
+    public AccountResource(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAccounts() {
+         return new ResponseEntity<>(accountService.getAllAccounts(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        return new ResponseEntity<>(accountService.createAccount(account), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable long id) {
+        return new ResponseEntity<>(accountService.getAccountById(id), HttpStatus.OK);
+    }
+}
---
+++ 22_quarkus/demo_kafka/micros/account-service/src/main/java/com/example/accountservice/service/AccountService.java
@@ -0,0 +1,37 @@
+package com.example.accountservice.service;
+
+import com.example.accountservice.entity.Account;
+import com.example.accountservice.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account getAccountById(Long id) {
+        return accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Not found account with id " + id));
+    }
+
+    public Account deacreaseBalance(Long id, Double amount) {
+        Account account = getAccountById(id);
+        account.setBalance(account.getBalance() - amount);
+        return accountRepository.save(account);
+    }
+}
