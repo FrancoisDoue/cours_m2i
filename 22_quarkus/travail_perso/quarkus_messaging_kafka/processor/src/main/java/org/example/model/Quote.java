050fd4377379653e02272a9233a7f57c74534867
FrancoisDoue <doue.francois@outlook.com>
2024-09-17 18:23:10 +0200

    before pull

---
+++ 22_quarkus/travail_perso/quarkus_messaging_kafka/processor/src/main/java/org/example/model/Quote.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+public class Quote {
+
+    private String id;
+    private int price;
+
+    public Quote() {}
+
+    public Quote(String id, int price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" + "id=" + id + ", price=" + price + '}';
+    }
+}
---
+++ 22_quarkus/travail_perso/quarkus_messaging_kafka/producer/src/main/java/org/example/kafka/producer/QuotesResources.java
@@ -0,0 +1,26 @@
+package org.example.kafka.producer;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import java.util.UUID;
+
+@Path("/quotes")
+public class QuotesResources {
+
+    @Channel("quote-request")
+    Emitter<String> quoteRequestEmitter;
+
+    @POST
+    @Path("/request")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createRequest() {
+        UUID uuid = UUID.randomUUID();
+        quoteRequestEmitter.send(uuid.toString());
+        return uuid.toString();
+    }
+}
